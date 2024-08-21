1fb72f35c9359cb0b8aa490f835f25a034235c50
Hui Wen <lovelyelsa@macbook-air-10.lan>
2024-08-20 17:27:57 -0700

    get the about links done

---
+++ react-vite/src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import './Footer.css';
+import { GrLinkedin } from "react-icons/gr";
+import { FaGithub } from "react-icons/fa";
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+                <div className='footer-author-name'>&copy; 2024 HUI WEN. All rights reserved.</div>
+                <div>
+                    <a href="https://github.com/Sally-HuiWen/solo-capstone" target="_blank" rel="noopener noreferrer">
+                      <FaGithub className='footer-github-icon'/> GitHub
+                    </a> 
+                    <a href="https://www.linkedin.com/in/hui-wen-best/" target="_blank" rel="noopener noreferrer">
+                      <GrLinkedin className='footer-linkedin-icon'/> LinkedIn
+                    </a>
+                </div>
+        </footer>
+    );
+}
